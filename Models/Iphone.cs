b2d4871f72ca608b57cbb434e3eeda0220bdd7e1
onedevmaster <jssmeca@hotmail.com>
2023-12-31 20:34:08 -0300

    Alteração dos

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+
+        // Construtor para inicializar as propriedades da classe base
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Implementação do método abstrato
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} na loja da Apple...");
+        }
+    
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor para inicializar as propriedades da classe base
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+         // Implementação do método abstrato
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} na loja da Nokia...");
+        }
     }
+
+
+    
 }
